3d86efb3826e302a7a6107cb0b6c06fe3f9240c6
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2019-08-05 19:15:45 +0800

    In progress on frontend Message APIs....

--- lib/backend/rest/message/MessageAPIService.dart
+++ lib/backend/rest/message/MessageAPIService.dart
@@ -1,7 +1,24 @@
-import 'package:snschat_flutter/environments/development/variables.dart' as globals;
+import 'dart:async';
 
+import 'dart:convert' as convert;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:snschat_flutter/environments/development/variables.dart'
+as globals;
+import 'package:snschat_flutter/objects/message/message.dart';
+
+import '../RestResponseUtils.dart';
 class MessageAPIService {
   String REST_URL = globals.REST_URL;
 
+  addMessage(Message message) async {
 
+  }
+
+  editMessage() async {}
+
+  deleteMessage() async {}
+
+  getMessagesOfAConversation() async {}
 }
