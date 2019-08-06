8e47a590c619c5957116df83454d7c1a4314b5f8
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2019-08-06 15:49:44 +0800

    Message API for front end is ready

--- lib/backend/rest/message/MessageAPIService.dart
+++ lib/backend/rest/message/MessageAPIService.dart
@@ -12,13 +12,66 @@ import '../RestResponseUtils.dart';
 class MessageAPIService {
   String REST_URL = globals.REST_URL;
 
-  addMessage(Message message) async {
+  Future<String> addMessage(Message message) async {
+    var httpResponse =
+    await http.post(REST_URL + "/message", body: message);
 
+    if (httpResponseIsOK(httpResponse)) {
+      var jsonResponse = convert.jsonDecode(httpResponse.body);
+      var newMessageId = jsonResponse['id'];
+      print("newMessageId: " + newMessageId);
+      newMessageId.id = newMessageId;
+      return newMessageId.id;
+    }
+    Fluttertoast.showToast(
+        msg: 'Unable to create a new message.Please try again.',
+        toastLength: Toast.LENGTH_SHORT);
+    return null;
   }
 
-  editMessage() async {}
+  Future<bool> editMessage(Message message) async {
+    var httpResponse =
+    await http.put(REST_URL + "/message", body: message);
+    if (httpResponseIsOK(httpResponse)) {
+      var jsonResponse = convert.jsonDecode(httpResponse.body);
+      var newMessageId = jsonResponse['id'];
+      print("newMessageId: " + newMessageId);
+      newMessageId.id = newMessageId;
+      return true;
+    }
 
-  deleteMessage() async {}
+    return false;
+  }
 
-  getMessagesOfAConversation() async {}
+  Future<bool> deleteMessage(Message message) async {
+    var httpResponse =
+    await http.delete(REST_URL + "/message" + message.id);
+    if (httpResponseIsOK(httpResponse)) {
+      var jsonResponse = convert.jsonDecode(httpResponse.body);
+      var newMessageId = jsonResponse['id'];
+      print("newMessageId: " + newMessageId);
+      message.id = newMessageId;
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<List<Message>> getMessagesOfAConversation(String conversationId) async {
+    var httpResponse =
+    await http.get(REST_URL + "/message/conversation/" + conversationId);
+
+    if (httpResponseIsOK(httpResponse)) {
+      var jsonResponse = convert.jsonDecode(httpResponse.body);
+      List<Message> messageList = convert.jsonDecode(jsonResponse);
+      print("messageList.length: " + messageList.length.toString());
+
+      return messageList;
+    }
+    Fluttertoast.showToast(
+        msg: 'Unable to get messages for this user.Please try again.',
+        toastLength: Toast.LENGTH_SHORT);
+    return null;
+
+  }
 }
