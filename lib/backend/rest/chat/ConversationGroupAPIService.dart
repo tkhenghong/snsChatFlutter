fe38431cef009cc7bf9eec612bae44230cbc4ba6
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-08-13 23:58:56 +0800

    In progress of Testing frontend APIs...

--- lib/backend/rest/chat/ConversationGroupAPIService.dart
+++ lib/backend/rest/chat/ConversationGroupAPIService.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-import 'package:snschat_flutter/environments/development/variables.dart'
-    as globals;
+import 'package:snschat_flutter/environments/development/variables.dart' as globals;
 import 'package:snschat_flutter/objects/chat/conversation_group.dart';
 
 import '../RestResponseUtils.dart';
@@ -13,9 +12,14 @@ class ConversationGroupAPIService {
   String REST_URL = globals.REST_URL;
 
   Future<Conversation> addConversation(Conversation conversation) async {
-    var httpResponse =
-        await http.post(REST_URL + "/conversationGroup", body: conversation);
+    print("ConversationGroupAPIService.addConversation()");
 
+    String wholeURL = REST_URL + "/conversationGroup";
+
+    print("wholeURL: " + wholeURL);
+    var httpResponse = await http.post(REST_URL + "/conversationGroup", body: conversation);
+
+    print("httpResponse: " + httpResponse.toString());
     if (httpResponseIsOK(httpResponse)) {
       var jsonResponse = convert.jsonDecode(httpResponse.body);
       // TODO: Find the new id returned by the server
@@ -28,25 +32,21 @@ class ConversationGroupAPIService {
   }
 
   Future<bool> editConversation(Conversation conversation) async {
-    var httpResponse =
-        await http.put(REST_URL + "/conversationGroup", body: conversation);
+    var httpResponse = await http.put(REST_URL + "/conversationGroup", body: conversation);
 
     return httpResponseIsOK(httpResponse);
   }
 
   Future<bool> deleteConversation(Conversation conversation) async {
-    var httpResponse =
-        await http.delete(REST_URL + "/conversationGroup" + conversation.id);
+    var httpResponse = await http.delete(REST_URL + "/conversationGroup" + conversation.id);
     return httpResponseIsOK(httpResponse);
   }
 
   Future<Conversation> getSingleConversation(String conversationGroupId) async {
-    var httpResponse =
-        await http.get(REST_URL + "/conversationGroup/" + conversationGroupId);
+    var httpResponse = await http.get(REST_URL + "/conversationGroup/" + conversationGroupId);
 
     if (httpResponseIsOK(httpResponse)) {
-      Conversation conversation =
-          convert.jsonDecode(httpResponse.body) as Conversation;
+      Conversation conversation = convert.jsonDecode(httpResponse.body) as Conversation;
       print("conversation.id: " + conversation.id);
       return conversation;
     }
@@ -54,12 +54,10 @@ class ConversationGroupAPIService {
   }
 
   Future<List<Conversation>> getConversationsForUser(String userId) async {
-    var httpResponse =
-        await http.get(REST_URL + "/conversationGroup/user/" + userId);
+    var httpResponse = await http.get(REST_URL + "/conversationGroup/user/" + userId);
 
     if (httpResponseIsOK(httpResponse)) {
-      List<Conversation> conversationList =
-          convert.jsonDecode(httpResponse.body) as List<Conversation>;
+      List<Conversation> conversationList = convert.jsonDecode(httpResponse.body) as List<Conversation>;
       print("conversationList.length: " + conversationList.length.toString());
       return conversationList;
     }
---
+++ test/ConversationGroupAPITest.dart
@@ -0,0 +1,35 @@
+import 'package:snschat_flutter/backend/rest/chat/ConversationGroupAPIService.dart';
+import 'package:snschat_flutter/objects/chat/conversation_group.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+
+  test("Test Create conversation", () async {
+    ConversationGroupAPIService conversationGroupAPIService = ConversationGroupAPIService();
+    print("Test start!");
+    Conversation conversation = new Conversation(
+      id: null,
+      name: "Testing Group 1",
+      description: "Testing description",
+      type: "Single",
+      createdDate: "107082019",
+      timestamp: "5643484641654",
+      notificationExpireDate: 254631654,
+      creatorUserId: "65421654654651",
+//      memberIds: ["wadwadw56f4sef", "56s4f6r54g89e4g", "54hs564ju456dyth5jsr", "5t4s5g1erg65t4ae"],
+      block: false,
+    );
+    print("conversation.id:" + conversation.id.toString());
+    print("conversation.name:" + conversation.name);
+    print("conversation.description:" + conversation.description);
+    print("conversation.type:" + conversation.type);
+    print("conversation.createdDate:" + conversation.createdDate);
+    print("conversation.timestamp:" + conversation.timestamp);
+
+
+    print("Executing test now....");
+    expect(await conversationGroupAPIService.addConversation(conversation), conversation);
+  });
+}
