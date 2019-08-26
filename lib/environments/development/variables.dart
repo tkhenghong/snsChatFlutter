aeedd3bdaec20920d440043062b26090da13c383
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2019-08-26 11:47:15 +0800

    Updating IP addresses of environment file

--- lib/environments/development/variables.dart
+++ lib/environments/development/variables.dart
@@ -1,4 +1,6 @@
 library snsChatFlutter.globals;
 
-String REST_URL = "http://192.168.0.139:8080";
+String REST_URL = "http://localhost:8080";
+// At house: http://192.168.0.139:8080
+// At Neurogine: http://192.168.88.183:8080
 String ENVIRONMENT = "DEVELOPMENT"; //DEVELOPMENT, PRODUCTION
