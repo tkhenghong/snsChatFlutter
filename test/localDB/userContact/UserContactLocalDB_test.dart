dc6072d09deaf4fade98dd3112111dc949792d57
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-08-27 00:26:54 +0800

    Added UserContact LocalDB tests

--- test/localDB/userContact/UserContactLocalDB_test.dart
+++ test/localDB/userContact/UserContactLocalDB_test.dart
@@ -1,10 +1,12 @@
 import 'package:snschat_flutter/backend/rest/userContact/UserContactAPIService.dart';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:snschat_flutter/database/sembast/userContact/userContact.dart';
 import 'package:snschat_flutter/objects/userContact/userContact.dart';
 
 void main() {
   UserContactAPIService userContactAPIService = UserContactAPIService();
+  UserContactDBService userContactDBService = UserContactDBService();
 
   UserContact createTestObject() {
     return new UserContact(
@@ -20,40 +22,66 @@ void main() {
 
   test("Test Create UserContact Locally", () async {
     UserContact userContact = createTestObject();
+    
     UserContact newUserContact = await userContactAPIService.addUserContact(userContact);
-    print("newUserContact.id:" + newUserContact.id.toString());
+    await userContactDBService.addUserContact(newUserContact);
+    UserContact userContactFromLocalDB = await userContactDBService.getSingleUserContact(newUserContact.id);
+    
     expect(newUserContact.id, isNotEmpty);
+    expect(userContactFromLocalDB.id, equals(newUserContact.id));
   });
 
   test("Test Edit UserContact Locally", () async {
     UserContact userContact = createTestObject();
+    
     UserContact newUserContact = await userContactAPIService.addUserContact(userContact);
+    await userContactDBService.addUserContact(newUserContact);
+    
     UserContact editedUserContact = newUserContact;
     editedUserContact.realName = "Teoh Kheng Lam";
     editedUserContact.displayName = "KL Lam";
     editedUserContact.mobileNo = "+60182223991";
     editedUserContact.userId = "999999999";
+    
     bool edited = await userContactAPIService.editUserContact(editedUserContact);
-    print("edited:" + edited.toString());
+    await userContactDBService.editUserContact(editedUserContact);
 
+    UserContact userContactFromLocalDB = await userContactDBService.getSingleUserContact(userContact.id);
+    
+    expect(userContactFromLocalDB.id, equals(editedUserContact.id));
+    expect(userContactFromLocalDB.realName, equals(editedUserContact.realName));
+    expect(userContactFromLocalDB.displayName, equals(editedUserContact.displayName));
+    expect(userContactFromLocalDB.mobileNo, equals(editedUserContact.mobileNo));
+    expect(userContactFromLocalDB.userId, equals(editedUserContact.userId));
     expect(edited, isTrue);
   });
 
   test("Test Get UserContact Locally", () async {
     UserContact userContact = createTestObject();
+    
     UserContact newUserContact = await userContactAPIService.addUserContact(userContact);
+    await userContactDBService.addUserContact(newUserContact);
+    
     UserContact userContactFromServer = await userContactAPIService.getUserContact(newUserContact.id);
-    print("userContactFromServer.id == newUserContact.id:" + (userContactFromServer.id == newUserContact.id).toString());
-    expect(userContactFromServer.id == newUserContact.id, isTrue);
+    UserContact userContactFromLocalDB = await userContactDBService.getSingleUserContact(userContact.id);
+    
+    expect(userContactFromServer.id, equals(newUserContact.id));
+    expect(userContactFromLocalDB.id, equals(userContactFromServer.id));
   });
 
   test("Test Delete UserContact Locally", () async {
     UserContact userContact = createTestObject();
+    
     UserContact newUserContact = await userContactAPIService.addUserContact(userContact);
-    print("newUserContact.id: " + newUserContact.id);
+    await userContactDBService.addUserContact(newUserContact);
+    
     bool deleted = await userContactAPIService.deleteUserContact(newUserContact.id);
+    await userContactDBService.deleteUserContact(userContact.id);
     print("deleted:" + deleted.toString());
+    
+    
     expect(deleted, isTrue);
+    expect(await userContactDBService.getSingleUserContact(userContact.id), null);
   });
 
   test("Test Get UserContacts from a Conversation Locally", () async {
