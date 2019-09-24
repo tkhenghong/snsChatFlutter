d75bc577169cdc166f6913d913c12b02e10c05e7
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2019-09-24 16:27:00 +0800

    Environment variables defaulted to Google Cloud VM

--- lib/environments/development/variables.dart
+++ lib/environments/development/variables.dart
@@ -1,6 +1,8 @@
 library snsChatFlutter.globals;
 
-String REST_URL = "http://localhost:8080";
-// At house: http://192.168.0.139:8080
-// At Neurogine: http://192.168.88.183:8080
-String ENVIRONMENT = "DEVELOPMENT"; //DEVELOPMENT, PRODUCTION
+String REST_URL = "http://35.184.11.203:8080";
+String WEBSOCKET_URL = "ws://35.184.11.203:8080/socket";
+// At Home: http://192.168.0.139:8080
+// At Neurogine: http://192.168.88.159:8080
+// In Google Cloud: http://35.184.11.203:8080
+String ENVIRONMENT = "PRODUCTION"; //DEVELOPMENT, PRODUCTION
