9ff5b16cb96d94784d8062f5c68d3596bc9ded4f
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2019-10-31 23:43:06 +0800

    Forgot to add IP GeoLocation API key

--- lib/environments/development/variables.dart
+++ lib/environments/development/variables.dart
@@ -1,8 +1,13 @@
 library snsChatFlutter.globals;
 
-String REST_URL = "http://35.184.11.203:8080";
-String WEBSOCKET_URL = "ws://35.184.11.203:8080/socket";
+String REST_URL = "http://192.168.88.188:8080";
+String WEBSOCKET_URL = "ws://192.168.88.188:8080/socket";
+//String REST_URL = "http://35.184.11.203:8080";
+//String WEBSOCKET_URL = "ws://35.184.11.203:8080/socket";
 // At Home: http://192.168.0.139:8080
 // At Neurogine: http://192.168.88.159:8080
 // In Google Cloud: http://35.184.11.203:8080
 String ENVIRONMENT = "PRODUCTION"; //DEVELOPMENT, PRODUCTION
+
+// Get Location of the device using IP address. https://ipgeolocation.io/
+String IP_GEOLOCATION_API_KEY  = "219609b60fdd4898a14c3f23a097aeea";
