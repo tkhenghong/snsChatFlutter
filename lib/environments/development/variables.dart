2c8b7968c2c9ecd874bbbb54cdd6d1b664424efc
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-12-28 00:18:07 +0800

    Added more variables for easier configurations

--- lib/environments/development/variables.dart
+++ lib/environments/development/variables.dart
@@ -9,5 +9,10 @@ String WEBSOCKET_URL = "ws://192.168.88.188:8080/socket";
 // In Google Cloud: http://35.184.11.203:8080
 String ENVIRONMENT = "PRODUCTION"; //DEVELOPMENT, PRODUCTION
 
+String DEFAULT_COUNTRY_CODE = "US";
+
 // Get Location of the device using IP address. https://ipgeolocation.io/
 String IP_GEOLOCATION_API_KEY  = "219609b60fdd4898a14c3f23a097aeea";
+
+int imagePickerQuality = 50;
+int imageThumbnailWidthSize = 50;
