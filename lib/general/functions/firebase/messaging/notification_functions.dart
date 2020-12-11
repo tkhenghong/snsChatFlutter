1a265b5fb25988b3e7284bdbde09d4787a817abc
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-12-11 19:24:29 +0800

    Created a function for Firebase notification on process the notification when the app is in background

---
+++ lib/general/functions/firebase/messaging/notification_functions.dart
@@ -0,0 +1,15 @@
+
+/// This happens when your app is in background(exited/stopped), when a notification comes, it can
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
+}
