f7a43ce3c3600372cf13b0c4866082b307c5e52a
Teoh Kheng Hong <tkhenghong@gmail.com>
2021-01-06 15:05:21 +0800

    Synchronize from other project in global variables, minor adjust quotes

--- lib/environments/development/variables.dart
+++ lib/environments/development/variables.dart
@@ -1,18 +1,21 @@
 library snsChatFlutter.globals;
-String REST_URL_HOST_IP = '192.168.68.109';
+String REST_URL_HOST_IP = '192.168.68.106';
 String REST_URL_HOST_PORT = '8888';
 
-String REST_URL = "https://$REST_URL_HOST_IP:$REST_URL_HOST_PORT";
-String WEBSOCKET_URL = "wss://$REST_URL_HOST_IP:$REST_URL_HOST_PORT/socket/websocket";
+String REST_URL = 'https://$REST_URL_HOST_IP:$REST_URL_HOST_PORT';
+String WEBSOCKET_URL = 'wss://$REST_URL_HOST_IP:$REST_URL_HOST_PORT/socket/websocket';
+
 String IP_GEO_LOCATION_HOST_ADDRESS = 'api.ipgeolocation.io';
 List<String> allowedHosts = [REST_URL_HOST_IP, IP_GEO_LOCATION_HOST_ADDRESS];
-// In Google Cloud: http://35.184.11.203:8080
-String ENVIRONMENT = "DEVELOPMENT"; //LOCAL_COMPUTER, DEVELOPMENT, PRODUCTION
 
-String DEFAULT_COUNTRY_CODE = "US";
+String ENVIRONMENT = 'LOCAL_COMPUTER'; // LOCAL_COMPUTER, DEVELOPMENT, PRODUCTION
+
+String sslCertificateLocation = 'lib/keystore/keystore.p12';
+
+String DEFAULT_COUNTRY_CODE = 'US';
 
 // Get Location of the device using IP address. https://ipgeolocation.io/
-String IP_GEOLOCATION_API_KEY = "219609b60fdd4898a14c3f23a097aeea";
+String IP_GEOLOCATION_API_KEY = '219609b60fdd4898a14c3f23a097aeea';
 
 int imagePickerQuality = 50;
 int imageThumbnailWidthSize = 50;
@@ -40,5 +43,23 @@ int emojiRows = 5;
 int emojiColumns = 7;
 int recommendedEmojis = 10;
 
+// Recaptcha
+String recaptchaDisabledKeyword = 'DISABLED';
+String recaptchaSecretKey = '6LcJCksUAAAAAC8lDh-Y9C0SfHQM5pA0TmkACCJy'; // Recaptcha Key from development server.
+String recaptchaSkipKey = '2dd1c2ad-5314-4fa4-8144-7ec27a7b3429-reCaptcha'; // Used as signal to tell backend to skip recaptcha.
+bool skipRecaptcha = true;
+
 // Pageable configurations (Pagination)
 int numberOfRecords = 20; // Control number of records that should be fetching every time a new page is pulled.
+
+int verificationPinMaximumRetryTimes = 3;
+
+// Allowed user roles to be logged into the app.
+List<String> allowedLoginUserRoles = ['ROLE_MER_ADMIN', 'ROLE_MER_VP', 'ROLE_MER_SUP', 'ROLE_MPOSCASHIER'];
+
+String copyrightYear = '2021 - 2022';
+String copyrightCompanyName = 'PocketChat';
+String copyrightCompanyLocation = 'Malaysia';
+String poweredByCompanyText= 'A.W.P.G.H.O.S.T';
+
+String versionNumber = '1.0.0';
