dd22815840cef5ed35cafdf512e625c4654defb7
Teoh Kheng Hong <teohkhenghong@Teohs-MacBook-Pro.local>
2021-02-20 17:35:38 +0800

    Added Google Maps dependency, integrate code into Android and iOS projects

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,6 +4,7 @@
 
 @import UIKit;
 @import Firebase;
+@import GoogleMaps;
 
 @implementation AppDelegate
 
@@ -15,6 +16,10 @@
    if(![FIRApp defaultApp]){
       [FIRApp configure];
   }
+
+  // Google Maps Key
+  [GMSServices provideAPIKey:@""];
+
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
